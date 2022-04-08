783915076547ff9a7b50026db8a0665dcfb5ede1
Philipp Moser <account@philipp-moser.de>
2022-04-08 10:33:18 +0200

    Setup EF Configuration

---
+++ src/LinkBakery.Core/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LinkBakery.Core.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+    }
+}
--- src/LinkBakery.Web.Redirect/Program.cs
+++ src/LinkBakery.Web.Redirect/Program.cs
@@ -1,4 +1,12 @@
+using LinkBakery.Core.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
 var app = builder.Build();
 
 app.MapGet("/", () => { });
